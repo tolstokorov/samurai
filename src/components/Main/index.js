1185f73c539560d10de6e38ffd6ca93476f89732
Sergey Tolstokorov <stolstokorov2020@yandex.ru>
2021-01-05 14:33:19 +0700

    fix stub structure in Main part

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,11 +9,13 @@ function Main() {
 
       <center>
         <div>ava + description</div>
-        <div>My posts</div>
-        <div>Nev post</div>
         <div>
-          <div>post 1</div>
-          <div>post 2</div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
         </div>
       </center>
       
